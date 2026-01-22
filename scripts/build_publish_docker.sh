9c335b1e23b4fdb457214c25f9c0a4231fb12c09
Gonçalo <g.j.teixeiradepinhoferreira@uva.nl>
2026-01-22 18:10:03 +0100

    Fixing docker build and publishing.

--- scripts/build_publish_docker.sh
+++ scripts/build_publish_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-docker build -t goncaloferreirauva/gd-wp6-kpi-service:latest -f ./kpi_service/Dockerfile ./kpi_service
+docker build -t goncaloferreirauva/gd-wp6-kpi-service:latest -f ./kpi_service/Dockerfile .
 docker push goncaloferreirauva/gd-wp6-kpi-service:latest
